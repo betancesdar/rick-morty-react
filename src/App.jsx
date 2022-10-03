3ad3beefed33b270a88bc79f5c74b71ffb2ffd16
Dario Betances <betancesdar08@gmail.com>
2022-10-02 22:04:34 -0400

    Master commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import ResidentCard from './components/ResidentCard'
-import useFetch from './hooks/useFetch'
 import getRandomNumber from './utils/getRandomNumber'
 import Locationinfo from './components/Locationinfo'
 import styled from 'styled-components'
+import axios from 'axios'
 
 const Container = styled.div`
   display:flex ;
@@ -15,15 +15,37 @@ const Container = styled.div`
 `
 
 function App() {
+  const [location, setLocation] = useState()
+  const [inputSearch, setinputSearch] = useState('')
 
-  const random = getRandomNumber()
-  const URL = `https://rickandmortyapi.com/api/location/${random}`
-  const location = useFetch(URL)
+  useEffect(() => {
+    let id = getRandomNumber()
+    if(inputSearch){
+      id = inputSearch
+    }
+
+    const URL = `https://rickandmortyapi.com/api/location/${id}`
+    
+    axios.get(URL)
+         .then(res => setLocation(res.data))
+         .catch(err => console.log(err))
+    
+  },[inputSearch])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setinputSearch(e.target.idLocation.value)
+  }
+
+  
 
 
   return (
     <div className="App">
+    <form onSubmit={handleSubmit}>
     <Navbar />
+    
+    </form>
     <Locationinfo location={location}/>
     <Container>
       {
